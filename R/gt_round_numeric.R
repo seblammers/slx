0751cfe357fca5fa497ad6d10ea1232047e933d0
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-01-25 12:14:34 +0100

    add check for character pv-column

--- R/gt_round_numeric.R
+++ R/gt_round_numeric.R
@@ -5,13 +5,31 @@ gt_round_numeric <- function(x) {
   # keep the existing numeric p-value for highlighting
   #
   # round all numeric values down to 2 decimals
+  #
+  # first of:
+  # check if the current tbl has a column named p.value
+  # that IS character (e.g. when receiving a tbl from
+  # afex::aov() -> afex::nice())
+  # if so convert to numeric and add the other column further below
+  # as we would if it were numeric to begin with
+  if (is.character(x$p.value)) {
+    x <- x %>%
+      mutate(
+        # extract only the numeric part
+        p.value = readr::parse_number(p.value)
+      )
+  }
+
   out <- x %>%
     mutate(
-      `p-value` = scales::pvalue(p.value),
+      # add a beautified labled version
+      # and keep the numeric one for highlighting purposes
+      p_value = scales::pvalue(p.value),
+      # round all numbers
       across(where(is.numeric), round, 2)
     ) %>%
     gt::gt() %>%
-    # hide the numeric version
+    # hide the numeric version of the p-values
     # it will not be displayed but can be used to highlight
     # where p <= .05
     gt::cols_hide(
